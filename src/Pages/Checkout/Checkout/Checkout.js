93c5df510abc33a1deeb0d1a9522e667e5d40093
Mohammed Jewel <jewelnabin@gmail.com>
2022-04-18 19:35:18 +0600

    style checkout page, also added a register form

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import './Checkout';
 const Checkout = () => {
     return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
+            <div class="container">
+                <div class="row">
+                    <h4>Click the following button and grab your fitness to fill up the Yoga Class Registration Form</h4>
+                    <div class="col text-center">
+                    <a href="https://form.jotform.com/221073552727454">
+                        <button style={{ background: '#d63384', color: 'white', border: 'none'}} className='btn-color btn-lg'>Register</button>
+                    </a>
+                    </div>
+                </div>
+            </div>
     );
 };
 
